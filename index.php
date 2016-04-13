2ed7ff6cf69bb76a935a23cd1506b9cfb80ac76b
mbart2 <matthewbarteau@gmail.com>
2016-04-12 20:48:36 -0400

    print

--- index.php
+++ index.php
@@ -1,73 +1,11 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact us</title>
-<!-- define some style elements-->
-<style>
-//adduser.php
-#popup {
-    visibility: hidden; 
-    background-color: red; 
-    position: absolute;
-    top: 10px;
-    z-index: 100; 
-    height: 100px;
-    width: 300px
-}
-h1
-{
-	font-family : Arial, Helvetica, sans-serif;
-	font-size : 16px;
-    font-weight : bold;
-}
-label,a 
-{
-	font-family : Arial, Helvetica, sans-serif;
-	font-size : 12px; 
-}
+<?php
 
-</style>	
-<!-- a helper script for vaidating the form-->
-<script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
-</head>	
-</head>
+$first_number = 10;
+$second_number = 20;
+$sum_total = $first_number + $second_number;
 
-<body>
-<div id="popup">
-    Record added successfully
-</div>
-<h1>Contact us</h1>
-<form method="POST" name="contactform" action="contact-form-handler.php"> 
-<p>
-<label for='name'>Your Name:</label> <br>
-<input type="text" name="name">
-</p>
-<p>
-<label for='email'>Email Address:</label> <br>
-<input type="text" name="email"> <br>
-</p>
-<p>
-<label for='message'>Message:</label> <br>
-<textarea name="message"></textarea>
-</p>
-<input type="submit" value="Submit"><br>
-</form>
+$direct_text = 'The two variables added together = ';
 
-<script language="JavaScript">
+print ($direct_text . $sum_total);
 
-// Code for validating the form
-// Visit http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
-// for details
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name"); 
-frmvalidator.addValidation("email","req","Please provide your email"); 
-frmvalidator.addValidation("email","email","Please enter a valid email address"); 
-</script>
-<!--
-Sample code from:
-http://www.html-form-guide.com/contact-form/php-email-contact-form.html
--->
-
-
-</body>
-</html>
+?>
