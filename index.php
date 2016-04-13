492b93a8ed03cecb054db2676e08a3663478fbba
mbart2 <matthewbarteau@gmail.com>
2016-04-12 20:50:14 -0400

    email

--- index.php
+++ index.php
@@ -8,4 +8,15 @@ $direct_text = 'The two variables added together = ';
 
 print ($direct_text . $sum_total);
 
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("someone@example.com","My subject",$msg);
+
+print ("sent");
+
 ?>
