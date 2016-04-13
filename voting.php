de61d21436c90919b6350ea996211a693192155f
mbart2 <matthewbarteau@gmail.com>
2016-04-12 21:21:44 -0400

    fad

--- voting.php
+++ voting.php
@@ -1,13 +1,5 @@
 <?php
-$london = $_POST['london']
-$paris = $_POST['paris']
-$prague = $_POST['prague']
-$zurich = $_POST['zurich']
-$venice = $_POST['venice']
-$berlin = $_POST['berlin']
-
-// the message
-$msg = "$london\n$paris\n$prague\n$zurich\n$venice\n$berlin\n";
+$msg = "First line of text\nSecond line of text";
 
 // send email
 mail("matthewbarteau@gmail.com","My subject",$msg);
