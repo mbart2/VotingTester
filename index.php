add0a734d8d92136bcaadd8ab6304c97080c7f21
mbart2 <matthewbarteau@gmail.com>
2016-04-12 21:17:47 -0400

    asdf

--- index.php
+++ index.php
@@ -1,108 +1,22 @@
-<form name="contactform" method="post" action="voting.php">
- 
-<table width="450px">
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="london">London</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="london" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
-<tr>
- 
- <td valign="top">
- 
-  <label for="paris">Paris</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="paris" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
-<tr>
- 
- <td valign="top">
- 
-  <label for="prague">Prague</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="prague" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
-<tr>
- 
- <td valign="top">
- 
-  <label for="zurich">Zurich</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="zurich" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
-<tr>
- 
- <td valign="top">
- 
-  <label for="venice">Venice</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="venice" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
-<tr>
- 
- <td valign="top">
- 
-  <label for="berlin">Berlin</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="berlin" maxlength="3" size="3">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td colspan="2" style="text-align:center">
- 
-  <input type="submit" value="Submit">
- 
- </td>
- 
-</tr>
- 
-</table>
- 
-</form>
+<?php
+
+$first_number = 10;
+$second_number = 20;
+$sum_total = $first_number + $second_number;
+
+$direct_text = 'The two variables added together = ';
+
+print ($direct_text . $sum_total);
+
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("matthewbarteau@gmail.com","My subject",$msg);
+
+print ("sent");
+
+?>
