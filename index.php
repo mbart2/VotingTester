de1d6005fae324338637e9ecf413b60f5d182140
mbart2 <matthewbarteau@gmail.com>
2016-04-15 18:39:10 -0400

    df

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if(isset($_POST['submit'])){
     $num1s = $_POST['num1s'];
     $num2s = $_POST['num2s'];
-    $message = $num1s . " " . $num2s . ":" . "\n\n" 
+    $message = $num1s . " " . $num2s . ":" . "\n\n" ;
 
 	mail("monaprakash7@gmail.com", "New Try", $message);
     echo "Mail Sent. Thank you, we will contact you shortly.";
