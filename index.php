165a1737d40b1eb9eec3354f879ba9e69ee04a2d
mbart2 <matthewbarteau@gmail.com>
2016-04-15 00:39:17 -0400

    a

--- index.php
+++ index.php
@@ -55,40 +55,6 @@
 </form>
  
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
- //this calculates values automatically
- sum();
- $("#num1, #num2, #num3, #num4, #num5, #num6").on("keydown keyup", function() {
- sum();
- });
-});
- 
-function sum() {
- var num1 = document.getElementById('num1').value;
- var num2 = document.getElementById('num2').value;
- var num3 = document.getElementById('num3').value;
- var num4 = document.getElementById('num4').value;
- var num5 = document.getElementById('num5').value;
- var num6 = document.getElementById('num6').value;
- var result = parseInt(num1) + parseInt(num2)+ parseInt(num3) + parseInt(num4)+ parseInt(num5) + parseInt(num6);
- var result1 = 100 - result;
- var num1s = Math.sqrt(num1);
- var num2s = Math.sqrt(num2);
- var num3s = Math.sqrt(num3);
- var num4s = Math.sqrt(num4);
- var num5s = Math.sqrt(num5);
- var num6s = Math.sqrt(num6);
- document.getElementById('num1s').value = num1s.toFixed(2);
- document.getElementById('num2s').value = num2s.toFixed(2);
- document.getElementById('num3s').value = num3s.toFixed(2);
- document.getElementById('num4s').value = num4s.toFixed(2);
- document.getElementById('num5s').value = num5s.toFixed(2);
- document.getElementById('num6s').value = num6s.toFixed(2);
- if (!isNaN(result)) {
- document.getElementById('subt').value = result1;
- }
- }
-</script>
+<script src="myScript.js"></script>
 </body>
 </html>
