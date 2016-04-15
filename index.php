adeeb882dad13a1444f5fced430d110814bbc6cd
mbart2 <matthewbarteau@gmail.com>
2016-04-15 11:50:31 -0400

    5

--- index.php
+++ index.php
@@ -44,11 +44,6 @@
 
 <tr> 
 	<td> HIIII: <\td>
-	<p class="pseudo_label">Some long winded yes or no question?</p>
-	<input type="radio" name="the_question" id="the_question_yes" value="1">
-	<label for="the_question_yes" class="after_radio">Yes</label>
-	<input type="radio" name="the_question" id="the_question_no" value="0">
-	<label for="the_question_no" class="after_radio">No</label>
 	</tr>
 	
 <tr>
