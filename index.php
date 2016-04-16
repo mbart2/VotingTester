bc904256dd55aec8550ea20a06ad997dc2c9a78a
mbart2 <matthewbarteau@gmail.com>
2016-04-15 22:49:49 -0400

    sd

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
-<?php 
-if(isset($_POST['submit'])){
-    $num1s = $_POST['num1s'];
-    $num2s = $_POST['num2s'];
-	$num3s = $_POST['num3s'];
-    $num4s = $_POST['num4s'];
-	$num5s = $_POST['num5s'];
-    $num6s = $_POST['num6s'];
-    $message = " London " . $num1s . " Paris " . $num2s . " Prague " . $num3s .  " Berlin " . $num4s . " Zurich " . $num5s . " Venice " . $num6s . "\n\n" ;
-
-	mail("monaprakash7@gmail.com", "Quadratic Votes", $message);
-    //echo "Mail Sent. Thank you, we will contact you shortly.";
-	header('Location: q1.html');
-    }
-?>
-
 <!DOCTYPE html>
 <head>
 <title>VacationVoting</title>
 </head>
 <body background="http://hdwallpaperia.com/wp-content/uploads/2014/01/Rainbow-Sky-Wallpaper-HD-Dekstop.jpg">
-<form name="form1" method="post" >
+<form name="form1" method="post" action="q1.html" >
 <table style="border-spacing: 10px;">
 <tr>
 	<td><b>City</b></td>
